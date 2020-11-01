8d338397f88e4896d2047887cf7ac47010e43e86
Vincent Gabb <vincentgabb@gmail.com>
2020-11-01 03:27:58 +0000

    almost done

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
-#include "functions.h"
-
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iomanip>
 using std::cout;
 using std::endl;
 using std::ifstream;
@@ -25,7 +24,60 @@ struct Country {
   double birthRate;
 };
 
+    const int name_width = 15 ;
+    const int int_width = 11 ;
+    const int dbl_width = 12 ;
+    const int num_flds = 7 ;
+    const std::string sep = " |" ;
+    const int total_width = name_width*2 + int_width*2 + dbl_width*3 + sep.size() * num_flds ;
+    const std::string line = sep + std::string( total_width-1, '-' ) + '|' ;
+
+    void header() {
+      cout << line << '\n' << sep
+        << std::setw(name_width) << "COUNTRY" << sep << std::setw(int_width) << "POPULATION" << sep
+        << std::setw(dbl_width) << "POP DENS" << sep << std::setw(dbl_width) << "NET MIG" << sep
+        << std::setw(int_width) << "GDP" << sep << std::setw(dbl_width) << "LITERACY" << sep
+        << std::setw(dbl_width) << "BIRTH RT" << sep << '\n' << line << '\n' ;
+    }
+
+void printItemList(Country c) {
+  cout << sep << std::setw(name_width) << c.name << sep << std::setw(int_width) << c.population << sep
+       << std::fixed << std::setprecision(2)
+       << std::setw(dbl_width) << c.popDensity << sep << std::setw(dbl_width) << c.netMigration << sep
+       << std::setw(int_width) << c.GDP << sep << std::setw(dbl_width) << c.literacy << sep
+       << std::setw(dbl_width) << c.birthRate << sep << '\n' ;
+}
+
+void clearScreen() {
+  cout << "\033[2J\033[1;1H";
+}
+
+bool sortByMostPopulous(const Country& x, const Country& y) { 
+  return x.population > y.population; 
+}
+
+bool sortByLeastPopulous(const Country& x, const Country& y) { 
+  return x.population < y.population; 
+}
+
+bool sortByMostArea(const Country& x, const Country& y) { 
+  return x.area > y.area; 
+}
+
+bool sortByLeastArea(const Country& x, const Country& y) { 
+  return x.area < y.area; 
+}
+
 int main() {
+
+    const int name_width = 15 ;
+    const int int_width = 7 ;
+    const int dbl_width = 12 ;
+    const int num_flds = 7 ;
+    const std::string sep = " |" ;
+    const int total_width = name_width*2 + int_width*2 + dbl_width*3 + sep.size() * num_flds ;
+    const std::string line = sep + std::string( total_width-1, '-' ) + '|' ;
+
 
 
   vector<Country> countryList(228);
@@ -97,6 +149,120 @@ int main() {
         } else {
           cout << "Unable to open countries.csv";
         }
-         dataViewer();
-       }
+
+  // data viewer starts
+  clearScreen();
+
+  cout << "[ WELCOME TO THE COUNTRY DATA VIEWER ]" << endl;
+  
+  cout << "\n - Press the ENTER key to continue - ";
+  if (cin.get() == '\n') {
+    clearScreen();
+  } 
+  
+  int sortingChoice;
+  while (sortingChoice != 0) {
+    cout << "View countries by..." << endl;
+
+    cout << "\n1. population" << endl;
+    cout << "2. area (square miles)" << endl;
+    cout << "3. population density (per square mile)" << endl;
+    cout << "4. net migration rate (per 1000)" << endl;
+    cout << "5. percapita GDP ($ per capita)" << endl;
+    cout << "6. literacy (%)" << endl;
+    cout << "7. birthrate (per 1000)" << endl;
+    cout << "0. EXIT program" << endl; 
+    cout << "\n- ";
+
+    cin >> sortingChoice;
+
+    clearScreen();
+
+    int howManyCountries(0);
+    int mostOrLeast(0);
+
+    if (sortingChoice == 1) {
+      cout << "How many countries? (1 - 10) : ";
+      cin >> howManyCountries;
+      cout << "1.) Most Populous, or 2.) Least Populous? : ";
+      cin >> mostOrLeast;
+      if (mostOrLeast == 1) {
+      sort(countryList.begin(), countryList.end(), sortByMostPopulous);
+      header();
+      for (int i = 0; i <= howManyCountries -1; i ++) {
+        
+        printItemList(countryList[i]);
+        }
+        // clears screen when user presses the enter key
+        cout << "\n - Press the ENTER key to continue - ";
+        cin.get();
+        if (cin.get() == '\n') {
+        clearScreen();
+        } 
+      } else if (mostOrLeast == 2) {
+        sort(countryList.begin(), countryList.end(), sortByLeastPopulous);
+        header();
+        for (int i = 0; i <= howManyCountries -1; i ++) {
+        printItemList(countryList[i]);
+        
+        }
+        // clears screen when user presses the enter key
+        cout << "\n - Press the ENTER key to continue - ";
+        cin.get();
+        if (cin.get() == '\n') {
+        clearScreen();
+        }
+      }
+
+    } else if (sortingChoice == 2) {
+      cout << "How many countries? (1 - 10)" << endl;
+      cin >> howManyCountries;
+      cout << "1.) Most Area, or 2.) Least Area?" << endl;
+      cin >> mostOrLeast;
+      if (mostOrLeast == 1) {
+      sort(countryList.begin(), countryList.end(), sortByMostArea);
+      header();
+      for (int i = 0; i <= howManyCountries -1; i ++) {
+        printItemList(countryList[i]);
+        
+        }
+      } else if (mostOrLeast == 2) {
+        sort(countryList.begin(), countryList.end(), sortByLeastArea);
+        header();
+        for (int i = 0; i <= howManyCountries -1; i ++) {
+        printItemList(countryList[i]);
+        
+        }
+      }
       
+    } else if (sortingChoice == 3) {
+      cout << "How many countries? (1 - 10)" << endl;
+      cin >> howManyCountries;
+      
+
+    } else if (sortingChoice == 4) {
+      cout << "How many countries? (1 - 10)" << endl;
+      cin >> howManyCountries;
+     
+
+    } else if (sortingChoice == 5) {
+      cout << "How many countries? (1 - 10)" << endl;
+      cin >> howManyCountries;
+   
+
+    } else if (sortingChoice == 6) {
+      cout << "How many countries? (1 - 10)" << endl;
+      cin >> howManyCountries;
+      
+
+    } else if (sortingChoice == 7) {
+      cout << "How many countries? (1 - 10)" << endl;
+      cin >> howManyCountries;
+      
+
+    } else if (sortingChoice == 0) {
+      //nothing happens so program ends
+    }
+  }
+}
+         
